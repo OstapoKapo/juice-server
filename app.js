702cba0fa74fbe492e6daf0bf1f7e2554ac8ba72
OstapoKapo <OstapoKapo@gmail.com>
2023-12-02 00:50:43 +0200

    start work

---
+++ app.js
@@ -0,0 +1,46 @@
+const {error} = require('console');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express();
+const PORT = 8000;
+const bodyParser = require('body-parser');
+const Products = require('./modules/products-model');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+}))
+
+app.get('/api', async (req,res) => {
+ res.json( await Products.find({}));
+})
+
+const uri = `mongodb+srv://ostapokapo:${process.env.TOKEN}@cluster.jrvydh9.mongodb.net/?retryWrites=true&w=majority`
+console.log(process.env.TOKEN)
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connected');
+})
+
+db.on('error', () => {
+    console.error(`MongoDB connection error: ${error}`)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server work on port ${PORT}`);
+})
+
